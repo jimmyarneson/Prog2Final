b20d0838b2c453f67d489a260fe17089eff57d2e
Owenhotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2024-05-17 14:35:06 -0500

    5/17

--- Prog2Final/Form3.cs
+++ Prog2Final/Form3.cs
@@ -32,10 +32,15 @@ namespace Prog2Final
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Random rand = new Random();
+            int num1 = 1;
+            if (num1 == 1) ;
+            listBox1.Items.Add("Alternating curls 3 sets x 8 reps\nAlternating barbell culrs 3 stes x 20 reps\nCable rope hammer curls 3 sets x 10 reps\nDumbbell kickback 3 sets x 10 reps");
 
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+
         {
 
         }
