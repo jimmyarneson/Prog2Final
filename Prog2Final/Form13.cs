8ad98ef549748f51687af103a1b0d585ba59f212
Owenhotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2024-05-21 14:35:53 -0500

    5/21

--- Prog2Final/Form13.cs
+++ Prog2Final/Form13.cs
@@ -29,5 +29,18 @@ namespace Prog2Final
         {
             Application.Exit();
         }
+        List<string> list = new List<string> { "Dumbbell Shoulder 3 x 10 reps" ,
+        "Pushups 5 x 20 reps", "Lateral Raise 3 x 10 reps", "Dumbell Front Raise 3 x 12 reps",
+            "Arnold Presses 3 x 8 reps", "Dumbell Lateral Raise 3 x 15 reps", "Military Press 2 sets of 10 at 25% bodyweight",
+            "Push Press 3 x 4-6 reps", "Reverse flys 3 x 15 reps", "Plank 1-2 mins" };
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            for (int lcv = 0; lcv < 5; lcv++) ;
+            int r = rnd.Next(list.Count);
+            listBox1.Items.Add(list[r]);
+           
+        }
     }
 }
